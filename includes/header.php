47f3a689e44f92cbc40d79f440b15b9263222901
Saurabh Aryal <saurabharyal10@gmail.com>
2024-02-23 17:29:06 +0545

    Initial Commit

---
+++ includes/header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="icon" href="images/favicon.png" type="image/gif" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Book Hub</title>
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="./css/bootstrap.css" />
+  <!-- font awesome style -->
+  <link href="./css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="./css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="./css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand ml-3" href="./">
+            <span>
+              Book Hub
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link pl-lg-0" href="./">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php"> About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="categories.php">Categories</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="books.php"> Books </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+            </ul>
+            <!-- <from class="search_form">
+              <input type="text" class="form-control" placeholder="Search here...">
+              <button class="" type="submit">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </from> -->
+          </div>
+        </nav>
+      </div>
+    </header>
+      <!-- end header section -->
+  <!-- </div> -->
