ca7df5a15e215e8818838653337ce87f114be349
Saurabh Aryal <saurabharyal10@gmail.com>
2024-03-28 07:33:53 +0545

    Initial Commit

--- books.php
+++ books.php
@@ -1,173 +1,93 @@
-<?php 
+<?php
+include_once('../book_store/connection/connection.php');
 include_once('includes/header.php');
+include_once('functions/function.php');
+
 ?>
 
-  <!-- blog section -->
+<!-- <div class="search-container">
+  <div class="navbar">
+    <input type="text" id="bookNameInput" placeholder="Search by book name...">
+    <div class="filters">
+      <label><input type="checkbox" name="category" value="romance"> Romance</label>
+      <label><input type="checkbox" name="category" value="course"> Course</label>
+      <label><input type="checkbox" name="category" value="fiction"> Fiction</label>
+      <label><input type="checkbox" name="category" value="novels"> Novels</label>
+      <label><input type="checkbox" name="category" value="motivational"> Motivational</label>
+    </div>
+    <button onclick="search()">Search</button>
+  </div>
+  <div id="searchResults"></div>
+</div> -->
 
-  <section class="blog_section layout_padding">
-    <div class="container">
-      <div class="heading_container heading_center">
-        <h2>
-          From Our Blog
-        </h2>
-      </div>
-      <div class="row">
-        <div class="col-md-6">
+<section class="blog_section layout_padding">
+  <div class="container">
+    <div class="heading_container heading_center">
+      <h2>
+        Searching For Book?
+      </h2>
+      <h4>
+        Jump Into the Search Bar
+      </h4>
+    </div>
+
+    <div class="book_category">
+      <h4 class="blog_date m-2 mt-5">
+        All Books
+      </h4>
+    </div>
+    <div class="row">
+      <?php
+      // Single Query 
+      //   $query = "SELECT * FROM tbl_books LIMIT 100";
+      // $result = $conn->query($query);
+      // while ($datas = $result->fetch_assoc()) {
+      //   var_dump($datas);
+      // }
+      // foreach ($datas as $data) {}
+      $query = "SELECT * FROM tbl_books";
+      $result = $conn->query($query);
+      while ($row = $result->fetch_assoc()) {
+        $datas[] = $row;
+      }
+      foreach ($datas as $data) {
+      ?>
+        <div class="col-md-3 col-6">
           <div class="box">
             <div class="img-box">
-              <img src="images/b1.jpg" alt="">
-              <h4 class="blog_date">
-                <span>
-                  19 January 2021
-                </span>
-              </h4>
+              <img src="system_panel/data_informations/books_images/<?php echo $data['book_image']; ?>" alt="">
             </div>
             <div class="detail-box">
-              <h5>
-                Eius, dolor? Vel velit sed doloremque
+              <h5 style=" text-align: center; margin-top: -30px;">
+                <?php
+                echo $data['book_name'];
+                ?>
               </h5>
               <p>
-                Incidunt magni explicabo ullam ipsa quo consequuntur eveniet illo? Aspernatur nam dolorem a neque? Esse eaque dolores hic debitis cupiditate, ad beatae voluptatem numquam dignissimos ab porro
+                <?php
+                $book_description =  $data['book_description'];
+                $truncated_description = truncateDescription($book_description, 45);
+                echo $truncated_description;
+                ?>
               </p>
+            </div>
+            <div class="read-more mb-3" style="text-align: center; margin-top: -35px;">
               <a href="">
-                Read More
+                Read More...
               </a>
             </div>
           </div>
         </div>
-        <div class="col-md-6">
-          <div class="box">
-            <div class="img-box">
-              <img src="images/b2.jpg" alt="">
-              <h4 class="blog_date">
-                <span>
-                  19 January 2021
-                </span>
-              </h4>
-            </div>
-            <div class="detail-box">
-              <h5>
-                Minus aliquid alias porro iure fuga
-              </h5>
-              <p>
-                Officiis veritatis id illo eligendi repellat facilis animi adipisci praesentium. Tempore ab provident porro illo ex obcaecati deleniti enim sequi voluptas at. Harum veniam eos nisi distinctio! Porro, reiciendis eius.
-              </p>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+
+      <?php
+      }
+      ?>
     </div>
-  </section>
 
-  <!-- end blog section -->
+  </div>
+</section>
 
-  <!-- info section -->
 
-  <section class="info_section layout_padding2">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-lg-3 info-col">
-          <div class="info_detail">
-            <h4>
-              About Us
-            </h4>
-            <p>
-              Vitae aut explicabo fugit facere alias distinctio, exem commodi mollitia minusem dignissimos atque asperiores incidunt vel voluptate iste
-            </p>
-            <div class="info_social">
-              <a href="">
-                <i class="fa fa-facebook" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-twitter" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-linkedin" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-instagram" aria-hidden="true"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3 info-col">
-          <div class="info_contact">
-            <h4>
-              Address
-            </h4>
-            <div class="contact_link_box">
-              <a href="">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                <span>
-                  Location
-                </span>
-              </a>
-              <a href="">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-                <span>
-                  Call +01 1234567890
-                </span>
-              </a>
-              <a href="">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <span>
-                  demo@gmail.com
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3 info-col">
-          <div class="info_contact">
-            <h4>
-              Newsletter
-            </h4>
-            <form action="#">
-              <input type="text" placeholder="Enter email" />
-              <button type="submit">
-                Subscribe
-              </button>
-            </form>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3 info-col">
-          <div class="map_container">
-            <div class="map">
-              <div id="googleMap"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end info section -->
-
-  <!-- footer section -->
-  <footer class="footer_section">
-    <div class="container">
-      <p>
-        &copy; <span id="displayYear"></span> All Rights Reserved By
-        <a href="https://html.design/">Free Html Templates</a>
-      </p>
-    </div>
-  </footer>
-  <!-- footer section -->
-
-  <!-- jQery -->
-  <script src="js/jquery-3.4.1.min.js"></script>
-  <!-- bootstrap js -->
-  <script src="js/bootstrap.js"></script>
-  <!-- custom js -->
-  <script src="js/custom.js"></script>
-  <!-- Google Map -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
-  </script>
-  <!-- End Google Map -->
-
-</body>
-
-</html>
+<?php
+include_once('includes/footer.php');
+?>
