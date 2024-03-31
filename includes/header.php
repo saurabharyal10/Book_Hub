332285cfbf55fd1c3411cc75ecd075f73d551d38
Saurabh Aryal <saurabharyal10@gmail.com>
2024-03-31 08:33:17 +0545

    Initial Commit

--- includes/header.php
+++ includes/header.php
@@ -25,52 +25,70 @@
   <!-- responsive style -->
   <link href="./css/responsive.css" rel="stylesheet" />
 
+  <style>
+    /* .fixed_header {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      z-index: 1000;
+      background-color: #ffffff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    } */
+
+    /* body {
+      padding-top: 80px;
+    } */
+  </style>
 </head>
 
 <body>
 
-  <div class="hero_area">
-    <!-- header section strats -->
-    <header class="header_section">
-      <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg custom_nav-container ">
-          <a class="navbar-brand ml-3" href="./">
-            <span>
-              Book Hub
-            </span>
-          </a>
+  <!-- <div class="hero_area"> -->
+  <!-- header section strats -->
+  <header class="header_section fixed_header">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg custom_nav-container ">
+        <a class="navbar-brand ml-3" href="./">
+          <span>
+            Book Hub
+          </span>
+        </a>
 
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class=""> </span>
-          </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class=""> </span>
+        </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link pl-lg-0" href="./">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.php"> About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="categories.php">Categories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="books.php"> Books </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact Us</a>
-              </li>
-            </ul>
-            <!-- <from class="search_form">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link pl-lg-0" href="./">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php"> About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="categories.php">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="books.php"> Books </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blogs.php"> Blogs </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact Us</a>
+            </li>
+          </ul>
+          <!-- <from class="search_form">
               <input type="text" class="form-control" placeholder="Search here...">
               <button class="" type="submit">
                 <i class="fa fa-search" aria-hidden="true"></i>
               </button>
             </from> -->
-          </div>
-        </nav>
-      </div>
-    </header>
-      <!-- end header section -->
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!-- end header section -->
   <!-- </div> -->
