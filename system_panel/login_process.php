35a86b791270f34d51fbc120e9a15eb1b7951ee6
Saurabh Aryal <saurabharyal10@gmail.com>
2024-04-02 08:11:16 +0545

    Initial Commit

---
+++ system_panel/login_process.php
@@ -0,0 +1,20 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$con = mysqli_connect("localhost", "root", "") or die("Cannot connect to the database");
+mysqli_select_db($con, "book_store");
+$query = "SELECT * FROM tbl_admin_login WHERE username='$username' and password='$password'";
+$result = mysqli_query($con, $query) or die(mysqli_error($con));
+$arr = mysqli_fetch_array($result, MYSQLI_ASSOC);
+if (isset($arr)) {
+  session_start();
+  $_SESSION['username'] = $username;
+  header('Location: index.php');
+  exit();
+} else {
+  // echo "You are not authorized user.";
+  $_SESSION['login_message'] = "Sorry, the credentials do not match.";
+  header("Location: " . $_SERVER['HTTP_REFERER']);
+}
